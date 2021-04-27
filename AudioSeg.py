077c969ebace39c3beeedad371f269f730bd3c8b
Hades300 <1018987488@qq.com>
2021-04-27 13:29:22 +0800

    wrap slice file by slience as function

--- AudioSeg.py
+++ AudioSeg.py
@@ -10,7 +10,6 @@ import pydub
 from datetime import datetime, timedelta
 
 
-wavfile.write
 # Utility functions
 def my_read(f, normalized=False):
     """MP3 to numpy array
@@ -99,88 +98,90 @@ silence_threshold = 1e-3
 step_duration = 0.03/10
 
 '''
-# Change the arguments and the input file here
-assert(len(sys.argv)==3)
-print(sys.argv)
-input_file = sys.argv[1]
-output_dir = sys.argv[2]
-min_silence_length = 0.3  # The minimum length of silence at which a split may occur [seconds]. Defaults to 3 seconds.
-silence_threshold = 1e-4  # The energy level (between 0.0 and 1.0) below which the signal is regarded as silent.
-step_duration = 0.03/10   # The amount of time to step forward in the input file after calculating energy. Smaller value = slower, but more accurate silence detection. Larger value = faster, but might miss some split opportunities. Defaults to (min-silence-length / 10.).
+
+def audio_split(input_file,output_dir):
+    # Change the arguments and the input file here
+    min_silence_length = 0.3  # The minimum length of silence at which a split may occur [seconds]. Defaults to 3 seconds.
+    silence_threshold = 1e-4  # The energy level (between 0.0 and 1.0) below which the signal is regarded as silent.
+    step_duration = 0.03/10   # The amount of time to step forward in the input file after calculating energy. Smaller value = slower, but more accurate silence detection. Larger value = faster, but might miss some split opportunities. Defaults to (min-silence-length / 10.).
 
 
-input_filename = input_file
-window_duration = min_silence_length
-if step_duration is None:
-    step_duration = window_duration / 10.
-else:
-    step_duration = step_duration
-
-output_filename_prefix = os.path.splitext(os.path.basename(input_filename))[0]
-dry_run = False
-
-print("Splitting {} where energy is below {}% for longer than {}s.".format(
-    input_filename,
-    silence_threshold * 100.,
-    window_duration
-    )
-)
-
-# Read and split the file
-
-# sample_rate, samples = input_data=wavfile.read(filename=input_filename, mmap=True)
-sample_rate, samples = input_data=my_read(input_filename)
-
-
-max_amplitude = np.iinfo(samples.dtype).max
-print(max_amplitude)
-
-max_energy = energy([max_amplitude])
-print(max_energy)
-
-window_size = int(window_duration * sample_rate)
-step_size = int(step_duration * sample_rate)
-
-signal_windows = windows(
-    signal=samples,
-    window_size=window_size,
-    step_size=step_size
-)
-
-window_energy = (energy(w) / max_energy for w in tqdm(
-    signal_windows,
-    total=int(len(samples) / float(step_size))
-))
-
-window_silence = (e > silence_threshold for e in window_energy)
-
-cut_times = (r * step_duration for r in rising_edges(window_silence))
-
-# This is the step that takes long, since we force the generators to run.
-print("Finding silences...")
-cut_samples = [int(t * sample_rate) for t in cut_times]
-cut_samples.append(-1)
-
-cut_ranges = [(i, cut_samples[i], cut_samples[i+1]) for i in range(len(cut_samples) - 1)]
-
-video_sub = {str(i) : [str(GetTime(((cut_samples[i])/sample_rate))), 
-                       str(GetTime(((cut_samples[i+1])/sample_rate)))] 
-             for i in range(len(cut_samples) - 1)}
-
-for i, start, stop in tqdm(cut_ranges):
-    output_file_path = "{}_{:03d}.mp3".format(
-        os.path.join(output_dir, output_filename_prefix),
-        i
-    )
-    if not dry_run:
-        print("Writing file {}".format(output_file_path))
-        # wavfile.write(
-        #     filename=output_file_path,
-        #     rate=sample_rate,
-        #     data=samples[start:stop]
-        # )
-        my_write(output_file_path,sample_rate,samples[start:stop])
+    input_filename = input_file
+    window_duration = min_silence_length
+    if step_duration is None:
+        step_duration = window_duration / 10.
     else:
-        print("Not writing file {}".format(output_file_path))
-with open (os.path.join(output_dir,output_filename_prefix+".json"), 'w') as output:
-    json.dump(video_sub, output)
+        step_duration = step_duration
+
+    output_filename_prefix = os.path.splitext(os.path.basename(input_filename))[0]
+    dry_run = False
+
+    print("Splitting {} where energy is below {}% for longer than {}s.".format(
+        input_filename,
+        silence_threshold * 100.,
+        window_duration
+        )
+    )
+
+    # Read and split the file
+
+    # sample_rate, samples = input_data=wavfile.read(filename=input_filename, mmap=True)
+    sample_rate, samples = input_data=my_read(input_filename)
+
+
+    max_amplitude = np.iinfo(samples.dtype).max
+    print(max_amplitude)
+
+    max_energy = energy([max_amplitude])
+    print(max_energy)
+
+    window_size = int(window_duration * sample_rate)
+    step_size = int(step_duration * sample_rate)
+
+    signal_windows = windows(
+        signal=samples,
+        window_size=window_size,
+        step_size=step_size
+    )
+
+    window_energy = (energy(w) / max_energy for w in tqdm(
+        signal_windows,
+        total=int(len(samples) / float(step_size))
+    ))
+
+    window_silence = (e > silence_threshold for e in window_energy)
+
+    cut_times = (r * step_duration for r in rising_edges(window_silence))
+
+    # This is the step that takes long, since we force the generators to run.
+    print("Finding silences...")
+    cut_samples = [int(t * sample_rate) for t in cut_times]
+    cut_samples.append(-1)
+
+    cut_ranges = [(i, cut_samples[i], cut_samples[i+1]) for i in range(len(cut_samples) - 1)]
+
+    video_sub = {str(i) : [str(GetTime(((cut_samples[i])/sample_rate))), 
+                        str(GetTime(((cut_samples[i+1])/sample_rate)))] 
+                for i in range(len(cut_samples) - 1)}
+
+    for i, start, stop in tqdm(cut_ranges):
+        output_file_path = "{}_{:03d}.mp3".format(
+            os.path.join(output_dir, output_filename_prefix),
+            i
+        )
+        if not dry_run:
+            print("Writing file {}".format(output_file_path))
+            # wavfile.write(
+            #     filename=output_file_path,
+            #     rate=sample_rate,
+            #     data=samples[start:stop]
+            # )
+            my_write(output_file_path,sample_rate,samples[start:stop])
+        else:
+            print("Not writing file {}".format(output_file_path))
+    with open (os.path.join(output_dir,output_filename_prefix+".json"), 'w') as output:
+        json.dump(video_sub, output)
+
+
+if __name__=="__main__":
+    audio_split(sys.argv[1],sys.argv[2])
